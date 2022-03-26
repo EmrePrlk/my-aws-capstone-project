069aba763c68caa9e810f67affaf755910a891fa
EmrePrlk <mr.prlk@gmail.com>
2022-03-26 14:31:21 +0300

    a

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"
+TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 git clone https://$TOKEN@github.com/EmrePrlk/my-aws-capstone-project.git
 cd /home/ubuntu/my-aws-capstone-project
 apt install python3-pip -y
